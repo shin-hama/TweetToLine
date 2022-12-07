d889e69466663f7e14e3bb965eb2beb975dc7a26
shin-hama <htysivista@gmail.com>
2022-12-07 00:45:49 +0000

    FEAT: check latest tweet is created at today

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timezone, timedelta
 import os
 
 from dotenv import load_dotenv
 from linebot import LineBotApi
 import tweepy
+from tweepy.models import Status
 from linebot.models import TextSendMessage
 
 load_dotenv()
@@ -11,21 +13,41 @@ load_dotenv()
 # 各認証情報を準備
 api_key = os.getenv("TWITTER_API_KEY")
 api_secret = os.getenv("TWITTER_API_SECRET")
-access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+twitter_access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+twitter_access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
 twitter_user_id = os.getenv("TWITTER_USER_ID")
 
 # API認証
 auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth.set_access_token(twitter_access_token, twitter_access_token_secret)
 
 twitter = tweepy.API(auth)
-result = twitter.user_timeline(id=twitter_user_id, count=1)
+result: list[Status] = twitter.user_timeline(id=twitter_user_id, count=1)
 
+# 最新のツイートを取得
+tweet = result[0]
+
+print(dir(tweet))
 tweet_text = result[0].text
+print(tweet.text)
+print(tweet.source_url)
+print(tweet.source)
+print(tweet.created_at)
+print(type(tweet.created_at))
 
-ACCESS_TOKEN = os.getenv("LINE_BOT_ACCESS_TOKEN")
 
-line_bot_api = LineBotApi(ACCESS_TOKEN)
+JST = timezone(timedelta(hours=9), "JST")
+now = datetime.now(JST)
+
+if isinstance(tweet.created_at, datetime) is False:
+    exit()
+
+delta = now - tweet.created_at
+if delta.days > 1:
+    exit()
+
+line_access_token = os.getenv("LINE_BOT_ACCESS_TOKEN")
+
+line_bot_api = LineBotApi(line_access_token)
 line_bot_api.broadcast(TextSendMessage(text=tweet_text))
